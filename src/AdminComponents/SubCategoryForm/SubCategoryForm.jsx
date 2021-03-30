1be2385cea12cf6a037ec4ab54cc2dff849e8afe
Osama Azhar <osamaazhar.edu@gmail.com>
2021-03-31 03:51:37 +0500

    Database model edited.

--- src/AdminComponents/SubCategoryForm/SubCategoryForm.jsx
+++ src/AdminComponents/SubCategoryForm/SubCategoryForm.jsx
@@ -6,68 +6,70 @@ const SubCategoryForm = () => {
             <form>
                 <h1>Add Category Form</h1>
                 <div className="flex">
-                <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div className="dropdown">
+  <button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
    Category
   </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
+  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a className="dropdown-item" href="#">Action</a>
+    <a className="dropdown-item" href="#">Another action</a>
+    <a className="dropdown-item" href="#">Something else here</a>
   </div>
 </div>
 
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<div className="dropdown">
+  <button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     Sub Category
   </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
+  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a className="dropdown-item" href="#">Action</a>
+    <a className="dropdown-item" href="#">Another action</a>
+    <a className="dropdown-item" href="#">Something else here</a>
   </div>
 </div>
                 </div>
 
 
-                <div class="form-group">
-    <label for="exampleInputEmail1">Enter Price</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+
+                <div className="form-group">
+    <label >Enter Product Name</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
   </div>
 
-  <div class="form-group">
-    <label for="exampleInputEmail1">Enter Product Name</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <div className="form-group">
+    <label >Enter Price</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
   </div>
 
 
-  <div class="form-group">
-    <label for="exampleInputEmail1">Quantity</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+
+  <div className="form-group">
+    <label >Quantity</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
   </div>
 
-  <div class="form-group">
-    <label for="exampleInputEmail1">Description</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  <div className="form-group">
+    <label >Description</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
   </div>
 
-  <div class="form-group">
-    <label for="exampleInputEmail1">Color</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  <div className="form-group">
+    <label >Color</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
   </div>
 
-  <div class="form-group">
-    <label for="exampleInputEmail1">Fabric</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  <div className="form-group">
+    <label>Fabric</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
   </div>
 
-  <div class="custom-file">
+  <div className="custom-file">
     <label>Select PRoduct Image</label>
-  <input type="file" class="custom-file-input" id="customFile"/>
-  <label class="custom-file-label" for="customFile">Choose file</label>
+  <input type="file" className="custom-file-input" id="customFile"/>
+  <label className="custom-file-label" >Choose file</label>
 </div>
 
-<button type="submit" class="btn btn-dark">Submit</button>
+<button type="submit" className="btn btn-dark">Submit</button>
             </form>
         </div>
     )
--- src/FirebaseUtilities/FirebaseUtilities.js
+++ src/FirebaseUtilities/FirebaseUtilities.js
@@ -7,7 +7,7 @@ import {firestore,storage} from "../Firebase/Firebase"
 //1) category name 2) Category Image 3) sub category name 4) sub category image
 export let createACategoryAndSubCategoryInFirestore = async(categoryObj) => {
     try {
-        let {mainCategoryCover,subCategoryCover,mainCategoryName,subCategoryName} = categoryObj;
+        let {mainCategoryCover,subCategoryCover,mainCategoryName,subCategoryName,id} = categoryObj;
 
         //had issues in file name while sending to firestore so had to edit it over here.
         categoryObj.mainCategoryCover = mainCategoryCover.name;
@@ -15,7 +15,7 @@ export let createACategoryAndSubCategoryInFirestore = async(categoryObj) => {
     
     
         //sending data to firestore making a separate collection for each category
-        await firestore.collection(`Categories`).doc(`${mainCategoryName}`).set(categoryObj);
+        await firestore.collection(mainCategoryName).doc(`${id}`).set(categoryObj);
     
         //creating image reference in storage
     
