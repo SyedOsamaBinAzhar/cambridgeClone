51a1496862930461d7e96df64a4be880ce8aed82
Osama Azhar <osamaazhar.edu@gmail.com>
2021-04-17 17:19:04 +0500

    video set in home new content

--- src/Components/HomeNewsContent/HomeNewsContent.js
+++ src/Components/HomeNewsContent/HomeNewsContent.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./HomeNewsContent.css"
-import WinterClearanceSale from "../../Assets/WinterClearanceSale.jpg"
+import VB_Desktop from "../../Assets/VB_Desktop.mp4"
+// import {} from "../../Assets"
 
 const HomeNewsContent = () => {
     return (
         <div className="homeCoverImage">
-                <img className="coverImage" src={WinterClearanceSale}></img>
+                {/* <img className="coverImage" src={WinterClearanceSale}></img> */}
+                <video width="100%" height="100%" controls >
+          <source src={VB_Desktop} type="video/mp4"/>
+        </video>
         </div>
     )
 }
